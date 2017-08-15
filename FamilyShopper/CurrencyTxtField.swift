6b048dc2b2523e417971d8ec8e395a16067bc67c
Ljubomir <ljubomir@LjubomirsMacbookAir.local>
2017-08-15 02:12:58 +0200

    Added dollar $ sign

--- FamilyShopper/CurrencyTxtField.swift
+++ FamilyShopper/CurrencyTxtField.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class CurrencyTxtField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8840534687, green: 0.8784657121, blue: 0.8883432746, alpha: 0.7970675616)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -16,7 +32,7 @@ class CurrencyTxtField: UITextField {
         layer.cornerRadius = 5.0
         textAlignment = .center
     
-        
+        clipsToBounds = true
     }
 
 }
